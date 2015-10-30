e95feb9da274a709ce92e8bea87ee62ed5b4c3f8
jacob-stone56 <jacobstone@Jacobs-MacBook-Pro.local>
2015-10-30 18:36:13 -0500

    initialize

--- ArduinoSerialTest/ArduinoSerialTest/main.cpp
+++ ArduinoSerialTest/ArduinoSerialTest/main.cpp
@@ -6,10 +6,91 @@
 //  Copyright © 2015 Jacob Stone. All rights reserved.
 //
 
+
+#include <stdio.h>
+#include <string.h>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define USB_SERIAL_PORT "/dev/tty.usbserial-A9030OKI" // Macbook Pro
+
+using namespace std;
+
+// Found online
+void init_port(int *fd){
+    struct termios options;
+    tcgetattr(*fd,&options);
+    
+    cfsetispeed(&options,B9600);    // set input baud rate
+    cfsetospeed(&options,B9600);    // set output baud rate
+    
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    tcsetattr(*fd,TCSANOW,&options);
+}
+
+int openFileDescriptor(int &fd){
+    // List usbSerial devices using Terminal ls /dev/tty.*
+    fd = open(USB_SERIAL_PORT, O_RDWR | O_NOCTTY | O_NDELAY);
+    
+    // Check for port errors
+    if(fd == -1) {
+        perror("Unable to open serial port\n");
+        return -1;
+    }
+    cout << "Serial port successfully opened\n";
+    cout << "Initializing usb port...\n";
     return 0;
+    
+}
+
+void handshake(int &fd, int initialization){
+    // Wait to recieve the initialization from arduino
+    cout << "Waiting for initialization signal from Arduino...\n";
+    while(initialization != 1)
+        read(fd, &initialization, sizeof(initialization));
+    cout << "Initialization signal recieved from Arduino\n";
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int fd;
+    int recieve = 0;
+    string input;
+    
+    if(openFileDescriptor(fd) == -1)
+        return -1;
+    
+    init_port(&fd);
+    sleep(1);           // give the port time to initialize
+    handshake(fd, 0);
+    
+    // just for testing purposes
+    for(int i = 0; i < 3; ++i){
+        cout << "Enter number between -127 and 127: ";
+        cin >> input;
+        write(fd, input.c_str(), sizeof(input));
+        
+        while(recieve == 0)
+            read(fd, &recieve, sizeof(recieve));
+        
+        if(recieve == 255)
+            recieve = 0;
+        else if(recieve > 128)
+            recieve = recieve - 256;
+        
+        cout << recieve << endl;
+        recieve = 0;
+        
+    }
+    
+    close(fd);
+    sleep(1);
+    return 1;
 }
