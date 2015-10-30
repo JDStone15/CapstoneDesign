adf09543f5da5d1a690e546e1ae1ce0e447ed2f2
jacob-stone56 <jacobstone@Jacobs-MacBook-Pro.local>
2015-10-30 18:34:31 -0500

    first commit

---
+++ ArduinoSerialTest/ArduinoSerialTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArduinoSerialTest
+//
+//  Created by Jacob Stone on 10/30/15.
+//  Copyright © 2015 Jacob Stone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
