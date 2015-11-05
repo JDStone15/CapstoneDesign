318c4eb2b3d2c1ee98c32ee7f29f210bbe55b9d0
Jacob Stone <jacob.stone56@tamu.edu>
2015-11-04 23:51:03 -0600

    Added a threshold feed
    Added a threshold feed to the scanning window. It has working sliders
    and spin boxes that work together to change the HSV values

--- scannerGUI/scanwindow.cpp
+++ scannerGUI/scanwindow.cpp
@@ -28,12 +28,28 @@ void ScanWindow::processFrameAndUpdateGui(){
     if(matOriginal.empty() == true)
         return;
 
+    // Convert the cameraFeed into a HSV colorspace
+    cvtColor(matOriginal, HSVFeed, COLOR_BGR2HSV);
+    if(HSVFeed.empty() == true)
+        return;
+    // Filter the HSV image and convert into our threshold cameraFeed
+    inRange(matOriginal, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
+    //cvtColor(HSVFeed, threshold, CV_HSV2RGB);
+    if(threshold.empty() == true)
+        return;
+
+    // QT works better in RGB not BGR
     cvtColor(matOriginal, matOriginal, CV_BGR2RGB);
 
     QImage qimgOriginal((uchar*)matOriginal.data, matOriginal.cols, matOriginal.rows, matOriginal.step, QImage::Format_RGB888);
+    QImage qimgThreshold((uchar*)threshold.data, threshold.cols, threshold.rows, threshold.step, QImage::Format_Grayscale8);
+ //   QImage::Format_Grayscale8
 
-    QImage small = qimgOriginal.scaled(512, 288, Qt::KeepAspectRatio);
-    ui->lbloriginal->setPixmap(QPixmap::fromImage(small));
+    QImage small1 = qimgOriginal.scaled(512, 288, Qt::KeepAspectRatio);
+    ui->lbloriginal->setPixmap(QPixmap::fromImage(small1));
+
+    QImage small2 = qimgThreshold.scaled(512, 288, Qt::KeepAspectRatio);
+    ui->lbloriginal_2->setPixmap(QPixmap::fromImage(small2));
 }
 
 
@@ -48,3 +64,15 @@ void ScanWindow::on_on_btnPauseOrResume_clicked(){
         ui->on_btnPauseOrResume->setText("pause");
     }
 }
+
+void ScanWindow::on_horizontalSlider_valueChanged(int value) { H_MIN = value; }
+
+void ScanWindow::on_horizontalSlider_2_valueChanged(int value) { S_MIN = value; }
+
+void ScanWindow::on_horizontalSlider_3_valueChanged(int value) { V_MIN = value; }
+
+void ScanWindow::on_horizontalSlider_6_valueChanged(int value) { H_MAX = value; }
+
+void ScanWindow::on_horizontalSlider_5_valueChanged(int value) { S_MAX = value; }
+
+void ScanWindow::on_horizontalSlider_4_valueChanged(int value) { V_MAX = value; }
--- scannerGUI/scanwindow.h
+++ scannerGUI/scanwindow.h
@@ -24,8 +24,18 @@ private:
 
     VideoCapture camera;
     Mat matOriginal;
+    Mat HSVFeed;
+    Mat threshold;
     QImage qimgOriginal;
     QTimer* tmrTimer;
+
+    int H_MIN = 0;
+    int S_MIN = 0;
+    int V_MIN = 0;
+
+    int H_MAX = 256;
+    int S_MAX = 256;
+    int V_MAX = 256;
 
 public slots:
     void processFrameAndUpdateGui();
@@ -34,6 +44,12 @@ public slots:
 private slots:
   //  void on_pushButton_clicked();
     void on_on_btnPauseOrResume_clicked();
+    void on_horizontalSlider_valueChanged(int value);
+    void on_horizontalSlider_2_valueChanged(int value);
+    void on_horizontalSlider_3_valueChanged(int value);
+    void on_horizontalSlider_6_valueChanged(int value);
+    void on_horizontalSlider_5_valueChanged(int value);
+    void on_horizontalSlider_4_valueChanged(int value);
 };
 
 #endif // SCANWINDOW_H
